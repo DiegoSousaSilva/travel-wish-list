f7fc212414f283b9b2921b0eb0c61afa0ce3fafe
Diego Sousa <derickbenji2@gmail.com>
2021-05-26 22:33:42 -0300

    Inicializando projeto através do boilerplate proprio

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,12 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Boilerplate NextJS</title>
+        <title>Travel Wish List</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta
           name="description"
-          content="Projeto com Typescript, React, NextJS, Styled Components"
+          content="Lista de lugares e desejos de viagem"
         />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
